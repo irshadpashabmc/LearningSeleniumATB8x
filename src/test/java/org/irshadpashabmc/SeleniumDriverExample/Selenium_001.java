34dee16f6a757fcecdad23a54ba302d78e210809
irshadpashabmc <irshadpasha.bmc@gmail.com>
2024-12-20 23:33:12 +0530

    Dec 20th 2024

--- src/test/java/org/irshadpashabmc/SeleniumDriverExample/Selenium_001.java
+++ src/test/java/org/irshadpashabmc/SeleniumDriverExample/Selenium_001.java
@@ -1,4 +1,21 @@
 package org.irshadpashabmc.SeleniumDriverExample;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class Selenium_001 {
+
+    @Test
+    public void FirstSeleniumTest() {
+
+        // System.setProperty("webdriver.chrome.driver","C://chrome.exe");
+        // ChromeDriver d = new ChromeDriver();
+        WebDriver Driver = new ChromeDriver();
+        Driver.get("http://www.google.com");
+        String a = Driver.getTitle();
+        System.out.println(a);
+
+    }
 }
