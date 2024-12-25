53c94fa0762573bb43ee0cfdcebbe8bd1d783237
irshadpashabmc <irshadpasha.bmc@gmail.com>
2024-12-25 09:57:45 +0530

    Dec 25th 2024 amending code and adding extra scripts-commiting task for demo-cura

--- src/test/java/org/irshadpashabmc/SeleniumDriverExample/ProjectCuraMakeAppointmentTest.java
+++ src/test/java/org/irshadpashabmc/SeleniumDriverExample/ProjectCuraMakeAppointmentTest.java
@@ -13,6 +13,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class ProjectCuraMakeAppointmentTest {
 
+    String CuraLoginText = "Login";
+    String CuraTitle = "Please login to make appointment.";
+    String AppointmentURL = "https://katalon-demo-cura.herokuapp.com/#appointment";
+
     @Description("Verify Cura Login Screen elements")
     @Test
     public void  CuraMakeAppointmentTest() throws InterruptedException {
@@ -24,17 +28,37 @@ public class ProjectCuraMakeAppointmentTest {
 
         WebDriver Driver = new ChromeDriver(CO);
 
-        Driver.get("https://katalon-demo-cura.herokuapp.com/profile.php#login");
+        // Open Make Appointment home Page
+
+        Driver.get("https://katalon-demo-cura.herokuapp.com/");
+        Driver.findElement(By.id("btn-make-appointment")).click();
+
+        // Page redirect to Dem-Cura Login Page
+        // Driver.get("https://katalon-demo-cura.herokuapp.com/profile.php#login");
+
+        // Verify LOGIN Text in the Cura-Login Page
+        WebElement VerifyLoginText = Driver.findElement(By.xpath("//*[@id=\"login\"]/div/div/div[1]/h2"));
+        assertThat(VerifyLoginText.getText()).isEqualTo(CuraLoginText);
+
+        // Verify Title text
+        WebElement VerifyCuraTitle = Driver.findElement(By.xpath("//*[@id=\"login\"]/div/div/div[1]/p"));
+        assertThat(VerifyCuraTitle.getText()).isEqualTo(CuraTitle);
+
+        // Verify Login with username and password
         Driver.findElement(By.id("txt-username")).sendKeys("John Doe");
         Driver.findElement(By.id("txt-password")).sendKeys("ThisIsNotAPassword");
-
         Driver.findElement(By.id("btn-login")).click();
         Thread.sleep(500);
+        System.out.println("This is Captured URL:"+Driver.getCurrentUrl());
+        assertThat(Driver.getCurrentUrl()).isEqualTo(AppointmentURL);
+        Driver.findElement(By.id("menu-toggle")).click();
 
-        System.out.println(Driver.getCurrentUrl());
-        assertThat(Driver.getCurrentUrl()).isEqualTo("https://katalon-demo-cura.herokuapp.com/#appointment");
+        // Wait and click on logout
+        Thread.sleep(500);
+        Driver.findElement(By.linkText("Logout")).click();
+        Driver.close();
+
+
 
     }
 }
-
-
